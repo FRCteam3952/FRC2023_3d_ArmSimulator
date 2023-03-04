9fbf96e1fe6116733bceae52bf10e7423a9c6f7e
Addison <addisonbchan@gmail.com>
2023-03-03 20:38:01 -0800

    Moved from manual arm position to moving a target location and having the arm automatically go to the target

--- src/main/java/frc/team3952/Armvisualiser.java
+++ src/main/java/frc/team3952/Armvisualiser.java
@@ -23,6 +23,49 @@ import com.jme3.system.AppSettings;
  * Move your Logic into AppStates or Controls or other java classes
  */
 public class Armvisualiser extends SimpleApplication {
+    /**
+     * JME's built-in rotation system doesn't suit our usage, so I've made my own rotation tracker
+     */
+    private double arm1AngleRad = Constants.ArmConstants.ARM_1_INITIAL_ANGLE * FastMath.DEG_TO_RAD, arm2AngleRad = Constants.ArmConstants.ARM_2_INITIAL_ANGLE * FastMath.DEG_TO_RAD;
+
+    // These need global scope for access in other locations.
+    private Geometry clawGeom, targetPointGeom;
+    private Node baseNode, arm1, arm2;
+    private BitmapText hudText;
+
+    /**
+     * The target position of the claw. Initialized to the claw's starting position.
+     */
+    private double targetX = 9.0297d, targetY = 29.1975d, targetZ = 0;
+
+    /**
+     * This handles keybinds to make changes appear on the model.
+     */
+    private final AnalogListener analogListener = (name, value, tpf) -> {
+        switch (name) {
+            case "X+":
+                targetX += 0.2;
+                break;
+            case "X-":
+                targetX -= 0.2;
+                break;
+            case "Y+":
+                targetY += 0.2;
+                break;
+            case "Y-":
+                targetY -= 0.2;
+                break;
+            case "Z+":
+                targetZ += 0.2;
+                break;
+            case "Z-":
+                targetZ -= 0.2;
+                break;
+        }
+        updateTarget();
+        updateHUDText();
+    };
+
     public static void main(String[] args) {
         Armvisualiser app = new Armvisualiser(); // Instantiate the app
         // app.showSettings = false; // This stops the settings screen from appearing. Only uncomment this line after you've set your initial settings once.
@@ -32,14 +75,22 @@ public class Armvisualiser extends SimpleApplication {
     }
 
     /**
-     * JME's built-in rotation system doesn't suit our usage, so I've made my own rotation tracker
+     * Rotates node Arm1
+     * @param angle radians
      */
-    private double arm1AngleRad = Constants.ArmConstants.ARM_1_INITIAL_ANGLE * FastMath.DEG_TO_RAD, arm2AngleRad = Constants.ArmConstants.ARM_2_INITIAL_ANGLE * FastMath.DEG_TO_RAD;
+    private void rotateArm1(double angle) {
+        arm1AngleRad += angle;
+        arm1.rotate(0, 0, (float) angle);
+    }
 
-    // These need global scope for access in other locations.
-    private Geometry clawGeom;
-    private Node baseNode, arm1, arm2;
-    private BitmapText hudText;
+    /**
+     * Rotates node Arm2
+     * @param angle radians
+     */
+    private void rotateArm2(double angle) {
+        arm2AngleRad -= angle; // bruh idk why but this needs to be done???
+        arm2.rotate(0, 0, (float) angle);
+    }
 
     /**
      * This function is run once on app startup. All initialization goes in here.
@@ -75,8 +126,12 @@ public class Armvisualiser extends SimpleApplication {
         Box clawB = new Box(1, 1, 1); // The claw (a box)
         clawGeom = new Geometry("Box", clawB);
 
+        Box targetPoint = new Box(1f, 1f, 1f); // The target point
+        targetPointGeom = new Geometry("Box", targetPoint);
+
         // This attaches items into the node system (consider it a tree). The items are linked specifically so I can rotate parts like the arm limbs at the same time.
         rootNode.attachChild(floorGeom);
+        rootNode.attachChild(targetPointGeom);
         rootNode.attachChild(baseNode);
         baseNode.attachChild(baseGeom);
         baseNode.attachChild(baseTowerGeom);
@@ -93,12 +148,12 @@ public class Armvisualiser extends SimpleApplication {
         baseNode.center(); // Make sure the base of the arm is at the origin
 
         // This moves the items into the correct position relative to each other
-        baseTowerGeom.move(0, ((float)Constants.ArmConstants.ORIGIN_HEIGHT * 1) / 2, 0);
-        arm1.move(0, ((float)Constants.ArmConstants.ORIGIN_HEIGHT * 1), 0);
-        arm2.move(0, ((float)Constants.ArmConstants.LIMB1_LENGTH * 1), 0);
-        claw.move(0, ((float)Constants.ArmConstants.LIMB2_LENGTH * 1), 0);
-        limb1Geom.move(0, ((float)Constants.ArmConstants.LIMB1_LENGTH * 1) / 2, 0);
-        limb2Geom.move(0, ((float)Constants.ArmConstants.LIMB2_LENGTH * 1) / 2, 0);
+        baseTowerGeom.move(0, ((float) Constants.ArmConstants.ORIGIN_HEIGHT * 1) / 2, 0);
+        arm1.move(0, ((float) Constants.ArmConstants.ORIGIN_HEIGHT * 1), 0);
+        arm2.move(0, ((float) Constants.ArmConstants.LIMB1_LENGTH * 1), 0);
+        claw.move(0, ((float) Constants.ArmConstants.LIMB2_LENGTH * 1), 0);
+        limb1Geom.move(0, ((float) Constants.ArmConstants.LIMB1_LENGTH * 1) / 2, 0);
+        limb2Geom.move(0, ((float) Constants.ArmConstants.LIMB2_LENGTH * 1) / 2, 0);
 
         // Set the rotation of the arm limbs to their initial positions. This code works
         arm1.rotate(0, 0, (float) Math.toRadians(180f + Constants.ArmConstants.ARM_1_INITIAL_ANGLE));
@@ -106,13 +161,14 @@ public class Armvisualiser extends SimpleApplication {
 
         // Colors the limbs in different colors
         Material mat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
-        Material mat2 = mat.clone(), mat3 = mat.clone(), mat4 = mat.clone(), mat5 = mat.clone(), mat6 = mat.clone(); // I was too lazy to keep initializing new materials
+        Material mat2 = mat.clone(), mat3 = mat.clone(), mat4 = mat.clone(), mat5 = mat.clone(), mat6 = mat.clone(), mat7 = mat.clone(); // I was too lazy to keep initializing new materials
         mat.setColor("Color", ColorRGBA.Blue);
         mat2.setColor("Color", ColorRGBA.Red);
         mat3.setColor("Color", ColorRGBA.Green);
         mat4.setColor("Color", ColorRGBA.Yellow);
         mat5.setColor("Color", ColorRGBA.Orange);
         mat6.setColor("Color", ColorRGBA.White);
+        mat7.setColor("Color", ColorRGBA.Magenta);
 
         DirectionalLight sun = new DirectionalLight(); // This is the light source for shadows
         sun.setDirection(new Vector3f(-0.5f, -0.5f, -0.5f).normalizeLocal()); // Positions the light source
@@ -132,6 +188,7 @@ public class Armvisualiser extends SimpleApplication {
         limb2Geom.setMaterial(mat4);
         clawGeom.setMaterial(mat5);
         floorGeom.setMaterial(mat6);
+        targetPointGeom.setMaterial(mat7);
 
         // Adds keybinds
         initKeys();
@@ -157,86 +214,90 @@ public class Armvisualiser extends SimpleApplication {
      * This method initializes the keybinds.
      */
     private void initKeys() {
-        inputManager.addMapping("Arm1+", new KeyTrigger(KeyInput.KEY_J));
-        inputManager.addMapping("Arm1-", new KeyTrigger(KeyInput.KEY_L));
-        inputManager.addMapping("Arm2+", new KeyTrigger(KeyInput.KEY_COMMA));
-        inputManager.addMapping("Arm2-", new KeyTrigger(KeyInput.KEY_PERIOD));
-        inputManager.addMapping("Turret+", new KeyTrigger(KeyInput.KEY_NUMPAD4));
-        inputManager.addMapping("Turret-", new KeyTrigger(KeyInput.KEY_NUMPAD6));
+        inputManager.addMapping("X+", new KeyTrigger(KeyInput.KEY_J));
+        inputManager.addMapping("X-", new KeyTrigger(KeyInput.KEY_L));
+        inputManager.addMapping("Y+", new KeyTrigger(KeyInput.KEY_COMMA));
+        inputManager.addMapping("Y-", new KeyTrigger(KeyInput.KEY_PERIOD));
+        inputManager.addMapping("Z+", new KeyTrigger(KeyInput.KEY_U));
+        inputManager.addMapping("Z-", new KeyTrigger(KeyInput.KEY_O));
 
-        inputManager.addListener(analogListener, "Arm1+", "Arm1-", "Arm2+", "Arm2-", "Turret+", "Turret-");
+        inputManager.addListener(analogListener, "X+", "X-", "Y+", "Y-", "Z+", "Z-");
     }
 
     /**
      * This method updates the HUD text. The text has a few modifications made to it to correct obvious errors caused by our 3D model (such as angles being negative).
      */
     private void updateHUDText() {
-        var tempTurretAngDeg = baseNode.getLocalRotation().toAngles(null);
+        double[] angles = getAnglesDeg();
 
-        // This is a bit of a hack, but since our arm1 limb is inverted, the rotation needs to be un-inverted properly.
-        // The other ones have other weird tidbits applied to them, but this works
-        double arm1AngleDeg = Math.abs(arm1AngleRad * FastMath.RAD_TO_DEG) % 360; // Math.abs((tempArm1AngDeg[2] * FastMath.RAD_TO_DEG) % 360) % 180;
-        double arm2AngleDeg = Math.abs(arm2AngleRad * FastMath.RAD_TO_DEG) % 360; // Math.abs(tempArm2AngDeg[2] * FastMath.RAD_TO_DEG);
-        double turretAngleDeg = (360 - tempTurretAngDeg[1] * FastMath.RAD_TO_DEG) % 360;
-
-        boolean isFlipped = arm2AngleDeg > 180;
-        if(arm1AngleDeg > 180) {
-            arm1AngleDeg = 360 - arm1AngleDeg;
+        boolean isFlipped = angles[1] > 180;
+        if (angles[0] > 180) {
+            angles[0] = 360 - angles[0];
         }
 
-        double[] fkuValues = ForwardKinematicsUtil.getCoordinatesFromAngles(arm1AngleDeg, arm2AngleDeg, turretAngleDeg);
+        double[] fkuValues = ForwardKinematicsUtil.getCoordinatesFromAngles(angles[0], angles[1], angles[2]);
 
-        var clawLocation = clawGeom.getWorldTranslation();
+        Vector3f clawLocation = clawGeom.getWorldTranslation(), targetLocation = targetPointGeom.getWorldTranslation();
 
         double[] ikuValues = InverseKinematicsUtil.getAnglesFromCoordinates(clawLocation.x, clawLocation.y, clawLocation.z, isFlipped);
 
         // Updates the text
-        hudText.setText("VALUES:\nIKU (DEG): " + ikuValues[0] + ", " + ikuValues[1] + ", " + ikuValues[2] + ", flipped: " + isFlipped
+        hudText.setText("VALUES:\nIKU (DEG): " + ikuValues[0] + ", " + ikuValues[1] + ", " + ikuValues[2]+ ", flipped: " + isFlipped
                 + "\nFKU (POS): " + fkuValues[0] + ", " + (42 + fkuValues[1]) + ", " + fkuValues[2]
-                + "\n\nREAL:\nANGLES (DEG): Arm1: " + arm1AngleDeg + ", Arm2: " + arm2AngleDeg + ", Turret: " + turretAngleDeg
+                + "\n\nREAL:\nANGLES (DEG): Arm1: " + angles[0] + ", Arm2: " + angles[1] + ", Turret: " + angles[2]
                 + "\nPOSITION: " + clawLocation.x + ", " + clawLocation.y + ", " + clawLocation.z
+                + "\nTARGET: " + targetLocation.x + ", " + targetLocation.y + ", " + targetLocation.z
         );
     }
 
     /**
-     * This handles keybinds to make changes happen on the model.
+     * Returns the known angles of the arm components.
+     *
+     * @return [arm1AngleDeg, arm2AngleDeg, turretAngleDeg]
      */
-    private final AnalogListener analogListener = new AnalogListener() {
-        @Override
-        public void onAnalog(String name, float value, float tpf) {
-            value *= 2f;
-            switch (name) {
-                case "Arm1+":
-                    arm1.rotate(0, 0, value);
-                    arm1AngleRad += value;
-                    updateHUDText();
-                    break;
-                case "Arm1-":
-                    arm1.rotate(0, 0, -value);
-                    arm1AngleRad -= value;
-                    updateHUDText();
-                    break;
-                case "Arm2+":
-                    arm2.rotate(0, 0, value);
-                    arm2AngleRad -= value;
-                    updateHUDText();
-                    break;
-                case "Arm2-":
-                    arm2.rotate(0, 0, -value);
-                    arm2AngleRad += value;
-                    updateHUDText();
-                    break;
-                case "Turret+":
-                    baseNode.rotate(0, value, 0);
-                    updateHUDText();
-                    break;
-                case "Turret-":
-                    baseNode.rotate(0, -value, 0);
-                    updateHUDText();
-                    break;
-            }
+    private double[] getAnglesDeg() {
+        var tempTurretAngDeg = baseNode.getLocalRotation().toAngles(null);
+
+
+        double arm1AngleDeg = Math.abs(arm1AngleRad * FastMath.RAD_TO_DEG) % 360; // Math.abs((tempArm1AngDeg[2] * FastMath.RAD_TO_DEG) % 360) % 180;
+        double arm2AngleDeg = Math.abs(arm2AngleRad * FastMath.RAD_TO_DEG) % 360; // Math.abs(tempArm2AngDeg[2] * FastMath.RAD_TO_DEG);
+        double turretAngleDeg = (360 - tempTurretAngDeg[1] * FastMath.RAD_TO_DEG) % 360;
+
+        return new double[]{arm1AngleDeg, arm2AngleDeg, turretAngleDeg};
+    }
+
+    /**
+     * This method updates the angles of the arm components using IKU to position the claw at the target position.
+     */
+    private void ikuUpdateAngles() {
+        double[] angles = getAnglesDeg();
+
+        if (angles[0] > 180) {
+            angles[0] = 360 - angles[0];
         }
-    };
+        if (angles[1] > 180) {
+            angles[1] = 360 - angles[1];
+        }
+
+        //var clawLocation = clawGeom.getWorldTranslation();
+        double[] ikuValues = InverseKinematicsUtil.getAnglesFromCoordinates(targetX, targetY, targetZ, false);
+
+        float adjustArm1AngRad = (float) Math.toRadians(360 - (angles[0] - ikuValues[0]));
+        float adjustArm2AngRad = (float) Math.toRadians(angles[1] - ikuValues[1]);
+        float adjustTurretAngRad = (float) Math.toRadians(angles[2] - ikuValues[2]);
+
+        baseNode.rotate(0, adjustTurretAngRad, 0);
+        rotateArm1(adjustArm1AngRad);
+        rotateArm2(adjustArm2AngRad);
+    }
+
+    /**
+     * Updates the target position and the arm angles to match.
+     */
+    private void updateTarget() {
+        targetPointGeom.center().move((float) targetX, (float) targetY, (float) targetZ);
+        ikuUpdateAngles();
+    }
 
     @Override
     public void simpleUpdate(float tpf) {
